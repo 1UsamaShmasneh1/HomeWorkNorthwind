cfecf842bdcba0911d48b1f7d137c31f15300809
usama shmasneh <us.05.07.91@gmail.com>
2021-12-29 22:34:38 +0200

    Add project files.

---
+++ HomeWorkNorthwind.ui/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace HomeWorkNorthwind.ui
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(-3, 187);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(1144, 438);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Question 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(166, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Question 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Question 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(166, 47);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Question 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 79);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(148, 29);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Question 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(166, 82);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(148, 29);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Question 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 114);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(148, 29);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Question 7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(166, 117);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(148, 29);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Question 8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 149);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(148, 29);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Question 9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(166, 152);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(148, 29);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Question 10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label
+            // 
+            this.label.AutoEllipsis = true;
+            this.label.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label.Location = new System.Drawing.Point(367, 19);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(765, 159);
+            this.label.TabIndex = 11;
+            this.label.Text = "label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 627);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ HomeWorkNorthwind.ui/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWorkNorthwind.ui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label.Text = "1. Write a query to get Product name and quantity/unit.";
+            using(var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select ProductName, QuantityPerUnit
+                //from Products
+                //where Discontinued = 'False'
+                //go
+                var prd = db.Products
+                    .Where(prod => prod.Discontinued == false)
+                    .Select(prod => new {prod.ProductName, prod.QuantityPerUnit})
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label.Text = "2.Write a query to get current Product list(Product ID and name).";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select ProductID, ProductName
+                //from Products
+                //order by ProductID
+                //go
+                var prd = db.Products
+                    .Select(prod => new { prod.ProductId, prod.ProductName })
+                    .OrderBy(prod => prod.ProductId)
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label.Text = "3.Write a query to get discontinued Product list(Product ID and name).";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select ProductName, ProductId
+                //from Products
+                //where Discontinued = 'true'
+                //order by ProductName
+                //go
+                var prd = db.Products
+                    .Where(prod => prod.Discontinued == true)
+                    .Select(prod => new { prod.ProductName, prod.ProductId })
+                    .OrderBy(prod => prod.ProductName)
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label.Text = "4.Write a query to get most expense to least expensive Product list(name and unit price).";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select ProductName, UnitPrice
+                //from Products
+                //order by UnitPrice desc
+                //go
+                var prd = db.Products
+                    .Select(prod => new { prod.ProductName, prod.UnitPrice })
+                    .OrderByDescending(prod => prod.UnitPrice)
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label.Text = "5.Write a query to get Product list(id, name, unit price) where current products cost less than $20.";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select ProductID, ProductName, UnitPrice
+                //from Products
+                //where UnitPrice < 20 AND Discontinued = 'False'
+                //order by ProductID
+                //go
+                var prd = db.Products
+                    .Where(prod => prod.UnitPrice < 20 &&  prod.Discontinued == false)
+                    .Select(prod => new {prod.ProductId, prod.ProductName, prod.UnitPrice })
+                    .OrderBy(prod => prod.ProductId)
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label.Text = "6.Write a query to get Product list(name, unit price) where products cost between $15 and $25.";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select ProductName, UnitPrice
+                //from Products
+                //where UnitPrice >= 15 And UnitPrice <= 25 and Discontinued = 'False'
+                //order by ProductName
+                //go
+                var prd = db.Products
+                    .Where(prod => prod.UnitPrice >= 15 && prod.UnitPrice <= 25 && prod.Discontinued == false)
+                    .Select(prod => new { prod.ProductName, prod.UnitPrice })
+                    .OrderBy(prod => prod.ProductName)
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label.Text = "7.Write a query to get Product list(id, name, unit price) of above average price.";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select distinct ProductName, UnitPrice
+                //from Products
+                //where UnitPrice > (select avg(UnitPrice) from Products)
+                //order by ProductName
+                //go
+                var prd = db.Products
+                    .Where(prod => prod.UnitPrice > db.Products.Average(prod => prod.UnitPrice))
+                    .Select(prod => new { prod.ProductName, prod.UnitPrice })
+                    .OrderBy(prod => prod.ProductName)
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label.Text = "8.Write a query to get Product list(name, unit price) of twenty most expensive products.";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select distinct top 20 ProductName, UnitPrice
+                //from Products
+                //order by UnitPrice desc
+                //go
+                var prd = db.Products
+                    .Select(prod => new { prod.ProductName, prod.UnitPrice })
+                    .OrderBy(prod => prod.ProductName)
+                    .Take(20)
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label.Text = "9.Write a query to count current and discontinued products.";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select count(ProductName)
+                //from Products
+                //group by Discontinued
+                //go
+                var prd = db.Products
+                    .Select ( prod => new
+                    {
+                        discountinued = db.Products.Where(prod => prod.Discontinued == true).Count(),
+                        current = db.Products.Where(prod => prod.Discontinued == false).Count()
+                    })
+                    .Take(1)
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label.Text = "10.Write a query to get Product list(name, units on order , units in stock) of stock is less than the quantity on order.";
+            using (var db = new HomeWorkNorthwind.ui.Models.NorthwindContext())
+            {
+                //select ProductName, UnitsOnOrder, UnitsInStock
+                //from Products
+                //where Discontinued = 'False' and UnitsInStock<UnitsOnOrder
+                //go
+                var prd = db.Products
+                    .Where(prod => prod.Discontinued == false && prod.UnitsInStock < prod.UnitsOnOrder)
+                    .Select(prod => new { prod.ProductName, prod.UnitsOnOrder, prod.UnitsInStock })
+                    .ToList();
+                dataGridView.DataSource = prd;
+            }
+        }
+    }
+}
